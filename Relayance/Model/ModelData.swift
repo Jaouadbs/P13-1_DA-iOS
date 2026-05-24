489a1b690ddf1c569cdd0f78248e09eacbbdfaa2
Jaouadbs <wk.boussalah@gmail.com>
2026-05-24 17:47:16 +0200

    Livrable 1: Modifie ModelData.swift (ajout du paramètre bundle) Ajout de ClientTests Ajout de DateExtensionTests Ajout de ModelDataTests

--- Relayance/Model/ModelData.swift
+++ Relayance/Model/ModelData.swift
@@ -3,25 +3,25 @@
 //  Relayance
 //
 //  Created by Amandine Cousin on 10/07/2024.
-//
+// Modifié pour permettre les tests unitaires (paramètre bundle)
 
 import Foundation
 
 struct ModelData {
-    static func chargement<T: Decodable>(_ nomFichier: String) -> T {
+    static func chargement<T: Decodable>(_ nomFichier: String, bundle: Bundle = .main) -> T {
         let data: Data
-        
-        guard let file = Bundle.main.url(forResource: nomFichier, withExtension: nil)
+
+        guard let file = bundle.url(forResource: nomFichier, withExtension: nil)
         else {
-            fatalError("Impossible de trouver \(nomFichier) dans le main bundle.")
+            fatalError("Impossible de trouver \(nomFichier) dans le  bundle.")
         }
-        
+
         do {
             data = try Data(contentsOf: file)
         } catch {
             fatalError("Impossible de charger \(nomFichier) depuis le main bundle:\n\(error)")
         }
-        
+
         do {
             let decoder = JSONDecoder()
             return try decoder.decode(T.self, from: data)
---
+++ Relayance/ViewModels/ClientsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ClientsViewModel.swift
+//  Relayance
+//
+//  Created by Jaouad on 23/05/2026.
+//
+
+import Foundation
